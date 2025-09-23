e9a1df5e02a71d03b7e2b79923b5c118bb636956
kourin <kourin.code@gmail.com>
2025-09-23 22:49:22 +0900

    Add print code

--- canoe/sp1-cc/host/src/lib.rs
+++ canoe/sp1-cc/host/src/lib.rs
@@ -158,9 +158,11 @@ async fn get_sp1_cc_proof(
         // Talked to sp1-cc developer already, and it is agreed.
         let is_valid = match &canoe_input.altda_commitment.versioned_cert {
             EigenDAVersionedCert::V2(_) => {
+                println!("EigenDAVersionedCert::V2");
                 Bool::abi_decode(&returns_bytes).expect("deserialize returns_bytes")
             }
             EigenDAVersionedCert::V3(_) => {
+                println!("EigenDAVersionedCert::V3");
                 let returns = <StatusCode as SolType>::abi_decode(&returns_bytes)
                     .expect("deserialize returns_bytes");
                 returns == StatusCode::SUCCESS
