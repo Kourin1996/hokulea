4f94851847bea459c1ef85102cf8130b47d92f95
kourin <kourin.code@gmail.com>
2025-09-24 17:12:12 +0900

    Add more log

--- canoe/sp1-cc/host/src/lib.rs
+++ canoe/sp1-cc/host/src/lib.rs
@@ -142,14 +142,14 @@ async fn get_sp1_cc_proof(
         let contract_input = match CertVerifierCall::build(&canoe_input.altda_commitment) {
             CertVerifierCall::V2(call) => {
                 println!(
-                    "\n\n!!! verifier_address={}, calldata={}!!!\n\n",
+                    "\n\n!!! verifier_address={}, calldata={:?}!!!\n\n",
                     canoe_input.verifier_address, call
                 );
                 ContractInput::new_call(canoe_input.verifier_address, Address::default(), call)
             }
             CertVerifierCall::Router(call) => {
                 println!(
-                    "\n\n!!! verifier_address={}, calldata={}!!!\n\n",
+                    "\n\n!!! verifier_address={}, calldata={:?}!!!\n\n",
                     canoe_input.verifier_address, call
                 );
                 ContractInput::new_call(canoe_input.verifier_address, Address::default(), call)
